4569fbdee23042b7524b21cadbf02482e3fa0be8
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2024-08-26 20:10:20 +0300

    farms

--- resources/views/farms_profile.blade.php
+++ resources/views/farms_profile.blade.php
@@ -59,53 +59,61 @@
 </head>
 <body>
     <div class="container">
-        
         <div class="card basic-info">
-        <div class="back-arrow" onclick="window.history.back()">&#8592; </div>
+            <div class="back-arrow" onclick="window.history.back()">&#8592;</div>
             <h2>Basic Farm Information</h2>
-           
+            
             <?php
             error_log($farm->profile_picture);
             $profilePicture = $farm->profile_picture ? asset('storage/' . $farm->profile_picture) : asset('storage/assets/farm.jpeg');
             ?>
             <img src="{{ $profilePicture }}" alt="profile">
-    
+
             <div class="details">
-                <span class="label">Farm name:</span> {{$farm->name}}           
+                <span class="label">Farm name:</span> {{ $farm->name }}
             </div>
             <div class="details">
                 <span class="label">Farm ownership:</span> {{ $farm->farmOwner->surname }} {{ $farm->farmOwner->given_name }}
             </div>
-
         </div>
+
         <div class="card additional-info">
             <h2>Farm Details</h2>
-            <div class="details">
-             
-             <span class="label">Farm type:</span> {{ implode(', ', $farm->livestock_type) }}
-             </div>
 
             <div class="details">
-             
-             <span class="label">Farm type:</span> {{ implode(', ', $farm->production_type) }}
-             </div>
-            <div class="details">
-                <span class="label">Date of establishment:</span> {{$farm->date_of_establishment}}
-            </div>
-            <div class="details">
-                <span class="label">Land size:</span> {{$farm->size}}
+                <span class="label">Farm type:</span>
+                @php
+                    $livestockType = is_string($farm->livestock_type) ? json_decode($farm->livestock_type, true) : $farm->livestock_type;
+                    echo is_array($livestockType) ? implode(', ', $livestockType) : ($livestockType ?: 'No livestock type specified');
+                @endphp
             </div>
 
             <div class="details">
-                <span class="label">Number of animals:</span> {{$farm->number_of_animals}}
+                <span class="label">Production type:</span>
+                @php
+                    $productionType = is_string($farm->production_type) ? json_decode($farm->production_type, true) : $farm->production_type;
+                    echo is_array($productionType) ? implode(', ', $productionType) : ($productionType ?: 'No production type specified');
+                @endphp
+            </div>
+
+            <div class="details">
+                <span class="label">Date of establishment:</span> {{ $farm->date_of_establishment }}
             </div>
             <div class="details">
-             
-             <span class="label">Farm structures:</span> {{ implode(', ', $farm->farm_structures) }}
-             </div>
+                <span class="label">Land size:</span> {{ $farm->size }}
+            </div>
+            <div class="details">
+                <span class="label">Number of animals:</span> {{ $farm->number_of_animals }}
+            </div>
 
+            <div class="details">
+                <span class="label">Farm structures:</span>
+                @php
+                    $farmStructures = is_string($farm->farm_structures) ? json_decode($farm->farm_structures, true) : $farm->farm_structures;
+                    echo is_array($farmStructures) ? implode(', ', $farmStructures) : ($farmStructures ?: 'No farm structures specified');
+                @endphp
+            </div>
         </div>
     </div>
-
 </body>
 </html>
